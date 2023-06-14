04d4440f30a25964e3a6ce7e21152422f6ecccc3
royossai <royossai@yahoo.com>
2023-06-14 14:17:19 +0200

    fixed the static path

--- dist/server.js
+++ dist/server.js
@@ -9,7 +9,7 @@ var path_1 = __importDefault(require("path"));
 var express_1 = __importDefault(require("express"));
 var router = new pondlive_1.Router();
 router.mount('/', views_1.Index);
-router.addStaticRoute(path_1.default.join(__dirname, './dist'));
+router.addStaticRoute(path_1.default.join(__dirname, './'));
 var app = router.serveWithExpress('/', (0, express_1.default)());
 app.listen(3000, function () {
     console.log('Server listening on port 3000');
